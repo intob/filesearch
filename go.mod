b69bb8fd930309d7eef20e06278c510f100644db
Joey Innes <avianpneuma@gmail.com>
2024-02-27 01:26:13 +0100

    target searching .xz packages

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module filesearch
 go 1.22.0
 
 require github.com/intob/jfmt v0.1.3
+
+require github.com/ulikunitz/xz v0.5.11
--- main.go
+++ main.go
@@ -2,89 +2,152 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"sync"
 
 	"github.com/intob/jfmt"
+	"github.com/ulikunitz/xz"
 )
 
+type result struct {
+	linesMatched, linesSearched uint32
+}
+
 func main() {
-	if len(os.Args) < 2 {
+	globFlag := flag.String("glob", "*", "file path glob")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
 		fmt.Println("expected argument for search text")
 		return
 	}
-	var total uint32
+	searchTxt := flag.Args()[0]
 
-	found := make(chan uint32, 1)
+	fmt.Printf("searching %s for \"%s\"\n", *globFlag, searchTxt)
+
+	results := make(chan *result, 1)
 	tasks := make(chan string, 1)
 
 	// gopher dispatches files as tasks
-	go func() {
-		files, err := filepath.Glob("*")
+	go func(glob string) {
+		files, err := filepath.Glob(glob)
 		if err != nil {
-			fmt.Println("Failed to glob files:", err)
+			fmt.Println("failed to glob files:", err)
 			return
 		}
 		for _, file := range files {
 			tasks <- file
 		}
 		close(tasks)
-	}()
+	}(*globFlag)
 
 	// 8 gophers complete the tasks
 	wg := &sync.WaitGroup{}
 	for i := 0; i < runtime.NumCPU()*2; i++ {
 		wg.Add(1)
-		go func() {
+		go func(searchTxt string) {
 			for task := range tasks {
-				searchFile(task, os.Args[1], found)
+				searchFile(task, searchTxt, results)
 			}
 			wg.Done()
-		}()
+		}(searchTxt)
 	}
 
 	go func() {
 		wg.Wait()
-		close(found)
+		close(results)
 	}()
 
-	var fileCount uint32
-	for count := range found {
-		total += count
-		fileCount++
-		fmt.Printf("\rsearched: %s, found: %s\033[0K",
-			jfmt.FmtCount32(fileCount),
-			jfmt.FmtCount32(total))
+	var linesMatchedTotal, linesSearchedTotal uint32
+	for result := range results {
+		linesMatchedTotal += result.linesMatched
+		linesSearchedTotal += result.linesSearched
+		fmt.Printf("\rlines searched: %s, lines contain \"%s\": %s\033[0K",
+			jfmt.FmtCount32(linesSearchedTotal),
+			searchTxt,
+			jfmt.FmtCount32(linesMatchedTotal))
 	}
 
 	fmt.Printf("\ndone")
-	if total == 0 {
-		fmt.Printf("\nnothing found for \"%s\"\n", os.Args[1])
+	if linesMatchedTotal == 0 {
+		fmt.Printf("\nnothing found for \"%s\"\n", searchTxt)
 	}
 }
 
 // searchFile searches for a given text in the specified file
-func searchFile(filePath, searchText string, found chan<- uint32) {
-	file, err := os.Open(filePath)
+func searchFile(filePath, searchText string, results chan<- *result) error {
+	var file *os.File
+	var err error
+	var tempPath string
+	if path.Ext(filePath) == ".xz" {
+		tempPath, err = decompressXZTemp(filePath)
+		if err != nil {
+			return fmt.Errorf("failed to decompress .xz: %w", err)
+		}
+		file, err = os.Open(tempPath)
+	} else {
+		file, err = os.Open(filePath)
+	}
 	if err != nil {
-		fmt.Printf("Error opening file %s: %v\n", filePath, err)
-		return
+		return fmt.Errorf("error opening file: %w", err)
 	}
 	defer file.Close()
+	defer func() {
+		if tempPath != "" {
+			err = os.Remove(tempPath)
+			if err != nil {
+				fmt.Println("failed to remove temp file:", tempPath, err)
+			}
+		}
+	}()
 
+	var linesMatched, linesSearched uint32
 	scanner := bufio.NewScanner(file)
-
-	var count uint32
 	for scanner.Scan() {
+		linesSearched++
 		if strings.Contains(scanner.Text(), searchText) {
-			count = 1
-			break // count at most once per file
+			linesMatched++
 		}
 	}
 
-	found <- count
+	results <- &result{
+		linesMatched:  linesMatched,
+		linesSearched: linesSearched,
+	}
+
+	return nil
+}
+
+// decompressXZ decompresses an .xz file.
+func decompressXZTemp(src string) (string, error) {
+	// Open the source .xz file
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return "", err
+	}
+	defer srcFile.Close()
+
+	// Create a reader to decompress the .xz file
+	reader, err := xz.NewReader(srcFile)
+	if err != nil {
+		return "", err
+	}
+
+	// Create the output file
+	destFile, err := os.CreateTemp("", "fsearch")
+	if err != nil {
+		return "", err
+	}
+	defer destFile.Close()
+
+	// Copy the decompressed data to the output file
+	_, err = io.Copy(destFile, reader)
+	return destFile.Name(), err
 }
